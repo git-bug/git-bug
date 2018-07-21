62c422fa96d9751903e8eeb8ff6bccc45eb5995a
Quentin Gliech <quentingliech@gmail.com>
2018-07-22 00:19:38 +0200

    Basic WebUI
    Based on Material-UI, react-router and react-apollo

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
-import './App.css'
+import React from "react";
+import { withRouter, Switch, Route } from "react-router";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Git bug</h1>
-        </header>
-        <p className="App-intro">
-          Here will appear a Web UI !
-        </p>
-      </div>
-    );
-  }
-}
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
-export default App;
+import Bug from "./Bug";
+
+const Home = () => <h1>Home</h1>;
+
+const App = ({ location }) => (
+  <React.Fragment>
+    <CssBaseline />
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="title" color="inherit">
+          git-bug-webui(1)
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/bug/:id" exact component={Bug} />
+    </Switch>
+  </React.Fragment>
+);
+
+export default withRouter(App);
--- webui/src/index.js
+++ webui/src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  connectToDevTools: true
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <React.Fragment>
+        <App />
+        <CssBaseline />
+      </React.Fragment>
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
