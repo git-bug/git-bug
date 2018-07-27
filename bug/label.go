ff2fd14e3f10a7206d4ec86f07e524cfa290e0fc
Michael Muré <batolettre@gmail.com>
2018-07-27 19:48:45 +0200

    wip gqlgen

--- bug/label.go
+++ bug/label.go
@@ -1,7 +1,29 @@
 package bug
 
+import (
+	"fmt"
+	"io"
+)
+
 type Label string
 
 func (l Label) String() string {
 	return string(l)
+}
+
+// UnmarshalGQL implements the graphql.Marshaler interface
+func (l *Label) UnmarshalGQL(v interface{}) error {
+	_, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("labels must be strings")
+	}
+
+	*l = v.(Label)
+
+	return nil
+}
+
+// MarshalGQL implements the graphql.Marshaler interface
+func (l Label) MarshalGQL(w io.Writer) {
+	w.Write([]byte(l))
 }
