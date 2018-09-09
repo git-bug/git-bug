877f3bc21ef6bb1ae636caaad8f62733f6de2f72
Michael Muré <batolettre@gmail.com>
2018-09-09 20:13:46 +0200

    status: add a function to parse a status

--- bug/status.go
+++ bug/status.go
@@ -1,5 +1,10 @@
 package bug
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Status int
 
 const (
@@ -27,5 +32,18 @@ func (s Status) Action() string {
 		return "closed"
 	default:
 		return "unknown status"
+	}
+}
+
+func StatusFromString(str string) (Status, error) {
+	cleaned := strings.ToLower(strings.TrimSpace(str))
+
+	switch cleaned {
+	case "open":
+		return OpenStatus, nil
+	case "closed":
+		return ClosedStatus, nil
+	default:
+		return 0, fmt.Errorf("unknow status")
 	}
 }
