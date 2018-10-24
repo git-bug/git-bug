85032d4cdc43b23da620d9e6b81cb961127566d5
Rafael Passos <rafael@rcpassos.me>
2018-10-24 18:55:02 -0300

    Inverted boolean check

--- util/interrupt/cleaner.go
+++ util/interrupt/cleaner.go
@@ -7,16 +7,17 @@ import (
 	"syscall"
 )
 
+// Cleaner type referes to a function with no inputs that returns an error
 type Cleaner func() error
 
 var cleaners []Cleaner
-var inactive bool
+var active = false
 
 // RegisterCleaner is responsible for regisreting a cleaner function. When a function is registered, the Signal watcher is started in a goroutine.
 func RegisterCleaner(f Cleaner) {
 	cleaners = append(cleaners, f)
-	if !inactive {
-		inactive = false
+	if !active {
+		active = true
 		go func() {
 			ch := make(chan os.Signal, 1)
 			signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
