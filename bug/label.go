93bed322faf962d75eb85360dcac362822e6cdb4
ludovicm67 <ludovicmuller1@gmail.com>
2019-03-28 11:25:42 +0100

    core: add color for label

--- bug/label.go
+++ bug/label.go
@@ -8,10 +8,50 @@ import (
 	"github.com/MichaelMure/git-bug/util/text"
 )
 
+type Color struct {
+	red uint8
+	green uint8
+	blue uint8
+}
+
 type Label string
 
 func (l Label) String() string {
 	return string(l)
+}
+
+func (l Label) Color() Color {
+	label := string(l)
+	id := 0
+
+	// colors from: https://material-ui.com/style/color/
+	colors := []Color{
+		Color{red: 244, green: 67, blue: 54}, // red
+		Color{red: 233, green: 30, blue: 99}, // pink
+		Color{red: 156, green: 39, blue: 176}, // purple
+		Color{red: 103, green: 58, blue: 183}, // deepPurple
+		Color{red: 63, green: 81, blue: 181}, // indigo
+		Color{red: 33, green: 150, blue: 243}, // blue
+		Color{red: 3, green: 169, blue: 244}, // lightBlue
+		Color{red: 0, green: 188, blue: 212}, // cyan
+		Color{red: 0, green: 150, blue: 136}, // teal
+		Color{red: 76, green: 175, blue: 80}, // green
+		Color{red: 139, green: 195, blue: 74}, // lightGreen
+		Color{red: 205, green: 220, blue: 57}, // lime
+		Color{red: 255, green: 235, blue: 59}, // yellow
+		Color{red: 255, green: 193, blue: 7}, // amber
+		Color{red: 255, green: 152, blue: 0}, // orange
+		Color{red: 255, green: 87, blue: 34}, // deepOrange
+		Color{red: 121, green: 85, blue: 72}, // brown
+		Color{red: 158, green: 158, blue: 158}, // grey
+		Color{red: 96, green: 125, blue: 139}, // blueGrey
+	}
+
+	for pos, char := range label {
+		id = (pos + id + int(char)) % len(colors)
+	}
+
+	return colors[id]
 }
 
 // UnmarshalGQL implements the graphql.Unmarshaler interface
