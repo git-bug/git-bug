209d337bbd2ca0b6d8535917dff9de77196938ae
ludovicm67 <ludovicmuller1@gmail.com>
2019-10-15 21:03:27 +0200

    bug: fix tests

--- bug/label_test.go
+++ bug/label_test.go
@@ -1,36 +1,35 @@
 package bug
 
 import (
-	"image/color"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestLabelRGBA(t *testing.T) {
-	rgba := Label("test").RGBA()
-	expected := color.RGBA{R: 255, G: 87, B: 34, A: 255}
+	rgba := Label("test").Color()
+	expected := LabelColor{R: 255, G: 87, B: 34, A: 255}
 
 	require.Equal(t, expected, rgba)
 }
 
 func TestLabelRGBASimilar(t *testing.T) {
-	rgba := Label("test1").RGBA()
-	expected := color.RGBA{R: 0, G: 188, B: 212, A: 255}
+	rgba := Label("test1").Color()
+	expected := LabelColor{R: 0, G: 188, B: 212, A: 255}
 
 	require.Equal(t, expected, rgba)
 }
 
 func TestLabelRGBAReverse(t *testing.T) {
-	rgba := Label("tset").RGBA()
-	expected := color.RGBA{R: 233, G: 30, B: 99, A: 255}
+	rgba := Label("tset").Color()
+	expected := LabelColor{R: 233, G: 30, B: 99, A: 255}
 
 	require.Equal(t, expected, rgba)
 }
 
 func TestLabelRGBAEqual(t *testing.T) {
-	color1 := Label("test").RGBA()
-	color2 := Label("test").RGBA()
+	color1 := Label("test").Color()
+	color2 := Label("test").Color()
 
 	require.Equal(t, color1, color2)
 }
