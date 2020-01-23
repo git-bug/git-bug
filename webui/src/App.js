f716bc1db9af25af96c59f9eee9c34525f7a88f0
ludovicm67 <ludovicmuller1@gmail.com>
2020-01-23 22:49:19 +0100

    webui: change primary color

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,5 +1,6 @@
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import React from 'react';
@@ -8,6 +9,14 @@ import { Link } from 'react-router-dom';
 
 import BugQuery from './bug/BugQuery';
 import ListQuery from './list/ListQuery';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#263238',
+    },
+  },
+});
 
 const useStyles = makeStyles(theme => ({
   appTitle: {
@@ -21,7 +30,7 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar position="static" color="primary">
         <Toolbar>
@@ -34,6 +43,6 @@ export default function App() {
         <Route path="/" exact component={ListQuery} />
         <Route path="/bug/:id" exact component={BugQuery} />
       </Switch>
-    </>
+    </ThemeProvider>
   );
 }
