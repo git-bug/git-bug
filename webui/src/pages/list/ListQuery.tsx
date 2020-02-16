c4f5cae4a44330ae0a8fb063768c4181fd0e83c1
Michael Muré <batolettre@gmail.com>
2020-02-16 01:35:51 +0100

    webui: list by default only open bugs

--- webui/src/pages/list/ListQuery.tsx
+++ webui/src/pages/list/ListQuery.tsx
@@ -164,7 +164,7 @@ function ListQuery() {
   const location = useLocation();
   const history = useHistory();
   const params = new URLSearchParams(location.search);
-  const query = params.get('q') || '';
+  const query = params.get('q') || 'status:open';
 
   const [input, setInput] = useState(query);
 
