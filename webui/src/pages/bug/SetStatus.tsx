218d460590a29fe84b94c024712a095e5f82f917
Michael Muré <batolettre@gmail.com>
2020-02-17 13:17:44 +0100

    webui: style SetStatus

--- webui/src/pages/bug/SetStatus.tsx
+++ webui/src/pages/bug/SetStatus.tsx
@@ -6,11 +6,15 @@ import Author from 'src/components/Author';
 import Date from 'src/components/Date';
 
 import { SetStatusFragment } from './SetStatusFragment.generated';
+import { Status } from '../../gqlTypes'
 
 const useStyles = makeStyles(theme => ({
   main: {
     ...theme.typography.body2,
     marginLeft: theme.spacing(1) + 40,
+  },
+  author: {
+    fontWeight: 'bold',
   },
 }));
 
@@ -20,10 +24,12 @@ type Props = {
 
 function SetStatus({ op }: Props) {
   const classes = useStyles();
+  const status = { [Status.Open]: 'reopened', [Status.Closed]: 'closed' }[op.status]
+
   return (
     <div className={classes.main}>
-      <Author author={op.author} bold />
-      <span> {op.status.toLowerCase()} this</span>
+      <Author author={op.author} className={classes.author} />
+      <span> {status} this </span>
       <Date date={op.date} />
     </div>
   );
