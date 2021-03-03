b918c9facd157d1dfb24282f6309782a10e02123
Sascha <GlancingMind@outlook.com>
2021-03-03 14:28:45 +0100

    Revert restructuring between App.tsx and index.tsx
    This should prevent unnecessary merge conflicts.

--- webui/src/index.tsx
+++ webui/src/index.tsx
@@ -1,13 +1,20 @@
 import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import apolloClient from './apollo';
+import Themer from './components/Themer';
+import { defaultLightTheme, defaultDarkTheme } from './themes/index';
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
-    <App />
+    <BrowserRouter>
+      <Themer lightTheme={defaultLightTheme} darkTheme={defaultDarkTheme}>
+        <App />
+      </Themer>
+    </BrowserRouter>
   </ApolloProvider>,
   document.getElementById('root')
 );
