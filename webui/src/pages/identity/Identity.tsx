1808ff432b48d10bc14eaf17471289d19f3e1b0d
Lena <lena.becker-3@mni.thm.de>
2021-03-12 02:06:09 +0100

    Add very badly styled user details #12

--- webui/src/pages/identity/Identity.tsx
+++ webui/src/pages/identity/Identity.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { MenuItem, MenuList } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { useCurrentIdentityQuery } from '../../components/CurrentIdentity/CurrentIdentity.generated';
@@ -11,7 +13,21 @@ const Identity = () => {
   const { loading, error, data } = useCurrentIdentityQuery();
   const user = data?.repository?.userIdentity;
   console.log(user);
-  return <main></main>;
+  return (
+    <main>
+      <h1>Profile</h1>
+      <Avatar src={user?.avatarUrl ? user.avatarUrl : undefined}>
+        {user?.displayName.charAt(0).toUpperCase()}
+      </Avatar>
+      <ul>
+        <li>Name: {user?.name ? user?.name : 'none'}</li>
+        <li title={user?.id}>Id: {user?.humanId ? user?.humanId : 'none'}</li>
+        <li>Email: {user?.email ? user?.email : 'none'}</li>
+        <li>Login: {user?.login ? user?.login : 'none'}</li>
+        <li>Protected: {user?.isProtected}</li>
+      </ul>
+    </main>
+  );
 };
 
 export default Identity;
