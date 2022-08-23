ff1b7448c9dab4a3723cb12ada49bc73ea7dc755
Steve Moyer <smoyer1@selesy.com>
2022-08-23 10:40:56 -0400

    fix(850): merge in CombinedId from 664

--- commands/comment_edit_test.go
+++ commands/comment_edit_test.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"testing"
 
-	"github.com/MichaelMure/git-bug/bug"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,20 +15,6 @@ func TestCommentEdit(t *testing.T) {
 		message: "this is an altered bug comment",
 	}
 	require.NoError(t, runCommentEdit(env.env, opts, []string{commentID}))
-
-	// TODO: remove this comment and everything between the "snip"
-	//       comments when issue #850 is resolved.
-
-	// ***** snip *****
-	cache, err := env.env.backend.ResolveBugPrefix(bugID)
-	require.NoError(t, err)
-	bu, err := bug.Read(env.env.repo, cache.Id())
-	require.NoError(t, err)
-	for _, op := range bu.Operations() {
-		t.Log("Operation: ", op)
-	}
-	t.Log("Compiled comments: ", bu.Compile().Comments)
-	// ***** snip *****
 
 	require.NoError(t, runComment(env.env, []string{bugID}))
 	requireCommentsEqual(t, golden, env)
