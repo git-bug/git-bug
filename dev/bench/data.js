29104a67145c0a76c8062a1665ce3270c01725ba
github-action-benchmark <github@users.noreply.github.com>
2022-12-08 11:29:03 +0000

    add Benchmark (go) benchmark result for 562d54802128dc8759f3e071c46f35cbbd6284f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669468445845,
+  "lastUpdate": 1670498943114,
   "repoUrl": "https://github.com/MichaelMure/git-bug",
   "entries": {
     "Benchmark": [
@@ -39,6 +39,48 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkReadBugs150",
             "value": 740724082,
             "unit": "ns/op\t840088524 B/op\t  834876 allocs/op",
+            "extra": "2 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "batolettre@gmail.com",
+            "name": "Michael Muré",
+            "username": "MichaelMure"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "562d54802128dc8759f3e071c46f35cbbd6284f7",
+          "message": "Merge pull request #941 from MichaelMure/dependabot/go_modules/golang.org/x/text-0.5.0\n\nbuild(deps): bump golang.org/x/text from 0.4.0 to 0.5.0",
+          "timestamp": "2022-12-08T12:27:24+01:00",
+          "tree_id": "f21a681bee1da3c8c95e09c34555900b31f3a087",
+          "url": "https://github.com/MichaelMure/git-bug/commit/562d54802128dc8759f3e071c46f35cbbd6284f7"
+        },
+        "date": 1670498942297,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReadBugs5",
+            "value": 27525421,
+            "unit": "ns/op\t25064172 B/op\t   25162 allocs/op",
+            "extra": "44 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkReadBugs25",
+            "value": 141762751,
+            "unit": "ns/op\t140655269 B/op\t  139677 allocs/op",
+            "extra": "8 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkReadBugs150",
+            "value": 794673324,
+            "unit": "ns/op\t840167388 B/op\t  834949 allocs/op",
             "extra": "2 times\n2 procs"
           }
         ]
