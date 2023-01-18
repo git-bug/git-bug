723b3c41470cafd240b0c2f174bc7e1762bc3680
Steve Moyer <smoyer1@selesy.com>
2023-01-17 20:35:59 -0500

    fix(commands): replace missing import

--- commands/bug/bug_comment_test.go
+++ commands/bug/bug_comment_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/MichaelMure/git-bug/commands/bug/testenv"
+	"github.com/MichaelMure/git-bug/commands/cmdtest"
 	"github.com/MichaelMure/git-bug/commands/execenv"
 )
 
