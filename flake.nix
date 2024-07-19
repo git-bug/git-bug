f7bf6f16ca43f71a4ef61ccdd4d01de41531044c
sudoforge <no-reply@sudoforge.com>
2024-07-18 21:01:04 -0700

    feat: add initial nix development shell
    This change bootstraps the configuration of a development shell via nix.
    
    Change-Id: Ic171e7599b50bb29ecc07d9c4534bcbc117f2299

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "workspace configuration for git-bug";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            codespell
+            gh
+            git
+            go
+            golangci-lint
+            nixfmt-rfc-style
+            nodePackages.prettier
+          ];
+        };
+      }
+    );
+}
