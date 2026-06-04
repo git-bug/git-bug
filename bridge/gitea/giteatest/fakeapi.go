ad7b812580076563c6b6dac5d9d3c09dc3c393fd
jyn <github@jyn.dev>
2026-06-04 12:54:18 +0200

    Add a bunch more iterator tests

--- bridge/gitea/giteatest/fakeapi.go
+++ bridge/gitea/giteatest/fakeapi.go
@@ -22,6 +22,10 @@ type FakeAPI struct {
 	Issues         []*gitea.Issue
 	Comments       []*gitea.Comment
 	CommentErrPage int // page number that returns 500; 0 = never
+	IssueErrPage   int // page number that returns 500 on issues endpoint; 0 = never
+	LabelErrPage   int // page number that returns 500 on labels endpoint; 0 = never
+
+	Labels []*gitea.Label
 
 	// NotFoundUsers is a list of usernames that return 404 from userGet.
 	NotFoundUsers []string
@@ -32,6 +36,9 @@ type FakeAPI struct {
 	// IssueRequests accumulates every request made to the issues endpoint.
 	// Inspect after running to verify query parameters such as `since`.
 	IssueRequests []*http.Request
+
+	// CommentRequests accumulates every request made to the comments endpoint.
+	CommentRequests []*http.Request
 }
 
 func writeJSON(w http.ResponseWriter, v any) {
@@ -47,18 +54,48 @@ func (fa *FakeAPI) NewServer(t *testing.T) *httptest.Server {
 
 	issuesPath := fmt.Sprintf("/api/v1/repos/%s/%s/issues", fa.Owner, fa.Project)
 	commentsPath := fmt.Sprintf("/api/v1/repos/%s/%s/issues/1/comments", fa.Owner, fa.Project)
+	labelsPath := fmt.Sprintf("/api/v1/repos/%s/%s/issues/1/labels", fa.Owner, fa.Project)
 
 	// https://codeberg.org/api/swagger#/issue/issueListIssues
 	mux.HandleFunc(issuesPath, func(w http.ResponseWriter, r *http.Request) {
 		fa.IssueRequests = append(fa.IssueRequests, r)
+
+		page := 1
+		if p := r.URL.Query().Get("page"); p != "" {
+			if n, err := strconv.Atoi(p); err == nil && n > 0 {
+				page = n
+			}
+		}
+		limit := 10
+		if l := r.URL.Query().Get("limit"); l != "" {
+			if n, err := strconv.Atoi(l); err == nil && n > 0 {
+				limit = n
+			}
+		}
+
+		if fa.IssueErrPage > 0 && page == fa.IssueErrPage {
+			http.Error(w, "simulated server error", http.StatusInternalServerError)
+			return
+		}
+
 		// X-Total-Count is stable but undocumented;
 		// see https://codeberg.org/forgejo/forgejo/issues/12931
 		w.Header().Set("X-Total-Count", strconv.Itoa(len(fa.Issues)))
-		writeJSON(w, fa.Issues)
+
+		start := (page - 1) * limit
+		end := start + limit
+		if start > len(fa.Issues) {
+			start = len(fa.Issues)
+		}
+		if end > len(fa.Issues) {
+			end = len(fa.Issues)
+		}
+		writeJSON(w, fa.Issues[start:end])
 	})
 
 	// https://codeberg.org/api/swagger#/issue/issueGetComments
 	mux.HandleFunc(commentsPath, func(w http.ResponseWriter, r *http.Request) {
+		fa.CommentRequests = append(fa.CommentRequests, r)
 		page := 1
 		if p := r.URL.Query().Get("page"); p != "" {
 			if n, err := strconv.Atoi(p); err == nil && n > 0 {
@@ -84,6 +121,37 @@ func (fa *FakeAPI) NewServer(t *testing.T) *httptest.Server {
 		}
 
 		writeJSON(w, fa.Comments[start:end])
+	})
+
+	// https://codeberg.org/api/swagger#/issue/issueGetLabels
+	mux.HandleFunc(labelsPath, func(w http.ResponseWriter, r *http.Request) {
+		page := 1
+		if p := r.URL.Query().Get("page"); p != "" {
+			if n, err := strconv.Atoi(p); err == nil && n > 0 {
+				page = n
+			}
+		}
+		limit := 10
+		if l := r.URL.Query().Get("limit"); l != "" {
+			if n, err := strconv.Atoi(l); err == nil && n > 0 {
+				limit = n
+			}
+		}
+
+		if fa.LabelErrPage > 0 && page == fa.LabelErrPage {
+			http.Error(w, "simulated server error", http.StatusInternalServerError)
+			return
+		}
+
+		start := (page - 1) * limit
+		end := start + limit
+		if start > len(fa.Labels) {
+			start = len(fa.Labels)
+		}
+		if end > len(fa.Labels) {
+			end = len(fa.Labels)
+		}
+		writeJSON(w, fa.Labels[start:end])
 	})
 
 	// https://codeberg.org/api/swagger#/miscellaneous/getVersion
